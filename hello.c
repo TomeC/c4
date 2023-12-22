827e04af256b4b516dba463a9a1ce4672b8a0d87
TomeC <wangkun_mr@foxmail.com>
2023-12-22 11:33:30 +0800

    格式化

--- hello.c
+++ hello.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
 
+int add(int a, int b)
+{
+	if (a > 1)
+	{
+		printf("a=%d\n", a);
+		return 1+add(a-1, b);
+	}
+  return a + b;
+}
 int main()
 {
-  printf("hello, world\n");
+  int a;
+  int b,c;
+
+ 	a=3;
+ 	  
+ 	b=2;
+  c = add(a, b);
+  printf("%d\n", c);
   return 0;
 }
